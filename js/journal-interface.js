4c0b6c8b0949a8c39ff06cf10a35385c953db12c
Drew Devlin and Dylan Stackhouse <drew@>
2017-03-13 11:11:46 -0700

    add getTeaser method to Entry

--- js/journal-interface.js
+++ js/journal-interface.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
     var wordCount = newEntry.getWordCount();
     var vowelCount = newEntry.getVowelCount();
     var consonantCount = newEntry.getConsonantCount();
+    var teaser = newEntry.getTeaser();
 
-    $('#entry-list').append("<h3>" + newEntry.jTitle + "</h3>" + "<p>" + newEntry.jBody + "</p>" + "<p>Word count: " + wordCount + "</p>" + "<p>Vowel count: " + vowelCount + "</p>" + "<p>Consonant count: " + consonantCount + "</p>");
+    $('#entry-list').append("<h3>" + newEntry.jTitle + "</h3>" + "<p>" + newEntry.jBody + "</p>" + "<p>Word count: " + wordCount + "</p>" + "<p>Vowel count: " + vowelCount + "</p>" + "<p>Consonant count: " + consonantCount + "</p><p> Teaser: " + teaser + "</p>");
   });
 });
--- js/journal.js
+++ js/journal.js
@@ -12,7 +12,6 @@ Entry.prototype = {
     var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
     this.jBody.toLowerCase().split("").forEach(function(letter) {
       if (vowels.indexOf(letter) !== -1) {
-
         count++;
       }
 
@@ -24,13 +23,28 @@ Entry.prototype = {
     var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
     this.jBody.toLowerCase().split("").forEach(function(letter) {
       if (consonants.indexOf(letter) !== -1) {
-        console.log(letter);
-        console.log(count);
-
         count++;
       }
 
     });
     return count;
+  }, getTeaser : function() {
+    var teaser = "";
+    var punctuation = this.jBody[this.jBody.search(/[^a-zA-Z\d\s:]/)];
+    console.log(this.jBody.search(/[^a-zA-Z\d\s:]/))
+    var sentence = this.jBody.split(/[^a-zA-Z\d\s:]/)[0].split(" ");
+
+    if(sentence.length < 8){
+      return sentence.join(" ") + punctuation;
+    }
+
+    for(var i = 0; i<8; i++){
+      var space = " ";
+      if(i === 7){
+        space = "";
+      }
+      teaser += sentence[i] + space;
+    }
+    return teaser + "...";
   }//end of prototypes
 }
